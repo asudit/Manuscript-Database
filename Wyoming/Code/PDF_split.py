f27db9c9d41110ab51f8db93e52232b931ee7d44
Adam Sudit <Adam@MacBook-Air.local>
2016-11-03 22:28:36 -0500

    pdf is split

--- Wyoming/Code/PDF_split.py
+++ Wyoming/Code/PDF_split.py
@@ -1,8 +1,19 @@
-import PyPDF2
+from PyPDF2 import PdfFileWriter, PdfFileReader
 import os, shutil, sys
 
 input_path = "D:\\Dropbox (Hornbeck Research)\\MFG Project\\manuscript_database\\WY\\Input\\Manufacturers Schedule, Census of Wyoming 1880.pdf"
 output_path = "D:\\Dropbox (Hornbeck Research)\\MFG Project\\manuscript_database\\WY\\Output"
 
-def split_pdf():
-	
+def split_pdf(input_path, output_path):
+	inputpdf = PdfFileReader(open(input_path, "rb"))
+	for i in xrange(inputpdf.numPages):
+		output = PdfFileWriter()
+		output.addPage(inputpdf.getPage(i))
+		with open(output_path + "\\" + "WY_8_%s.pdf" % i, "wb") as outputStream:
+			output.write(outputStream)
+
+
+
+######################################################################
+if __name__ == '__main__':
+	split_pdf(input_path, output_path)
