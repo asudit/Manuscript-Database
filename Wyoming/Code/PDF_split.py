de9b009a0bd5c954f95960972d6152c025a0cdea
Adam Sudit <Adam@MacBook-Air.local>
2016-11-03 22:13:17 -0500

    Wyoming first push

---
+++ Wyoming/Code/PDF_split.py
@@ -0,0 +1,8 @@
+import PyPDF2
+import os, shutil, sys
+
+input_path = "D:\\Dropbox (Hornbeck Research)\\MFG Project\\manuscript_database\\WY\\Input\\Manufacturers Schedule, Census of Wyoming 1880.pdf"
+output_path = "D:\\Dropbox (Hornbeck Research)\\MFG Project\\manuscript_database\\WY\\Output"
+
+def split_pdf():
+	
